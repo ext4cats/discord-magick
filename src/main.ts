e8991264edd515291911900429962f8c37784e64
freeram <kulikovpavel2801@gmail.com>
2024-08-11 18:55:49 -0600

    Add tweetnakl, discord Interactions Endpoint handling

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,61 @@
+import verifyDiscordRequest from './verifyDiscordRequest.js';
+
+export interface Env {
+  DISCORD_PUBLIC_KEY: string;
+}
+
+interface Interaction {
+  type: number;
+  data?: InteractionData;
+}
+
+interface InteractionData {
+  name: string;
+}
+
+async function handleRequest(request: Request, env: Env): Promise<Response> {
+  const isValidRequest = await verifyDiscordRequest(
+    request,
+    env.DISCORD_PUBLIC_KEY,
+  );
+  if (!isValidRequest) {
+    return new Response('Invalid request signature', { status: 401 });
+  }
+
+  let body: Interaction;
+  try {
+    body = (await request.json()) as Interaction;
+  } catch {
+    return new Response('Invalid JSON body', { status: 400 });
+  }
+
+  // DEBUG
+  console.log(body);
+
+  if (body.type === 1) {
+    return new Response(JSON.stringify({ type: 1 }), {
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+
+  if (body.type === 2 && body.data) {
+    const { name } = body.data;
+    switch (name) {
+      case 'ping': {
+        return new Response(
+          JSON.stringify({ type: 4, data: { content: 'Pong!' } }),
+          {
+            headers: { 'Content-Type': 'application/json' },
+          },
+        );
+      }
+    }
+  }
+  return new Response('Unknown request type', { status: 400 });
+}
+
 export default {
-  async fetch() {
-    return Response.json({ msg: 'Hello, world!' });
+  async fetch(request: Request, env: Env) {
+    return handleRequest(request, env);
   },
-} satisfies ExportedHandler;
+} satisfies ExportedHandler<Env>;
