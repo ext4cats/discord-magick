af1af13fa95b608b00bfc2f7169a7e1d45c61038
freeram <kulikovpavel2801@gmail.com>
2024-08-12 23:53:30 -0600

    Refuse to forget to delete old files

--- src/discordResponse.ts
+++
@@ -1,9 +0,0 @@
-export default class DiscordResponse extends Response {
-  constructor(body: object) {
-    super(JSON.stringify(body), {
-      headers: {
-        'Content-Type': 'application/json;charset=UTF-8',
-      },
-    });
-  }
-}
--- src/interaction.ts
+++
@@ -1,9 +0,0 @@
-export default interface Interaction {
-  type: number;
-  data?: InteractionData;
-}
-
-interface InteractionData {
-  name: string;
-  // resolved?: ResolvedData;
-}
