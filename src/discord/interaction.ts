a9cd9ee671b9714c744c97188a5be1045ce5c8c9
freeram <kulikovpavel2801@gmail.com>
2024-08-16 01:27:38 -0600

    Add interaction processing logic

--- src/discord/interaction.ts
+++ src/discord/interaction.ts
@@ -1,9 +1,23 @@
-export default interface Interaction {
-  type: number;
-  data?: InteractionData;
+interface Attachment {
+  id: string;
+  filename: string;
+  size: number;
+  url: string;
+  proxy_url: string;
+  content_type?: string;
 }
 
-interface InteractionData {
-  name: string;
-  // resolved?: ResolvedData;
+interface Message {
+  attachments: Attachment[];
+}
+
+export default interface Interaction {
+  type: number;
+  data: {
+    target_id: string;
+    name: string;
+    resolved: {
+      messages: Record<string, Message>;
+    };
+  };
 }
